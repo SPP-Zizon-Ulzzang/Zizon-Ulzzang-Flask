4374f3c01d060dd3ffbdb73c45a3311379eecaa9
juns1s <juns1s@naver.com>
2023-07-19 13:55:47 -0700

    fix: 예외처리

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pickle
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 import warnings
 from instagramUtils import InstagramUtils as IUtils
 import instagram_crawl as crawler
@@ -15,9 +15,14 @@ util = IUtils()
 
 
 def run_algorithm(url):
-    print(url)
+    # text = util.post_by_user(url)
+    text = util.post_by_post_url(url)
 
-    text = util.post_by_user(url)
+    print("====================")
+    print("text: ", text)
+
+    if len(text)<1:
+        raise RuntimeError("비공개 계정 또는 텍스트가 존재하지 않습니다.")
 
     # 모델을 통한 예측
     predicted_labels = model.predict(text)
@@ -30,17 +35,22 @@ def run_algorithm(url):
 @app.route('/instagram', methods=['GET'])
 def predict():
     # POST 요청에서 데이터 추출
-    snsUrl = request.args.get("snsUrl")
+    sns_url = request.args.get("snsUrl")
+
+    print("sns_url: ",sns_url)
 
     # url을 통한 크롤링 후 데이터를 기반으로 분류 실행
-    # try:
-    result = run_algorithm(snsUrl)
-    # except Exception as ex:
-    #     print(ex)
-    #     response = {
-    #         "mbti": "error"
-    #     }
-    #     return jsonify(response)
+    try:
+        result = run_algorithm(sns_url)
+    except RuntimeError as e:
+        message = e.args
+        response = make_response(message, 400)
+        return response
+    except Exception as e:
+        print(e.args)
+        message = "크롤링 중 오류가 발생했습니다."
+        response = make_response(message, 500)
+        return response
 
     response = {
         "mbti": str(result)
