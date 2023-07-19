73d888cb7efdda256adfd5b7392f63b9b7fe5cb9
juns1s <juns1s@naver.com>
2023-07-19 13:54:47 -0700

    refactor: 최대 조회 게시글 수 30개로 변 및 중복 코드 삭

--- instagramUtils.py
+++ instagramUtils.py
@@ -3,35 +3,25 @@ import os
 from dotenv import load_dotenv
 from instagrapi import Client
 
-def instagram_login():
-    load_dotenv()
 
-    INSTA_ID = os.environ.get("INSTA_ID")
-    INSTA_PW = os.environ.get("INSTA_PW")
-
-    cl = Client()
-    # 인스타그램 로그인
-    cl.login(INSTA_ID, INSTA_PW)
-
-    return cl
 class InstagramUtils:
     cl = Client()
+
     def __int__(self):
         load_dotenv()
 
         INSTA_ID = os.environ.get("INSTA_ID")
         INSTA_PW = os.environ.get("INSTA_PW")
-
         # 인스타그램 로그인
         self.cl.login(INSTA_ID, INSTA_PW)
 
-    def post_by_user(self, userName):
+    def post_by_user(self, usern_name):
         # 인스타 게시물 수 by username(id)
-        media_count = self.cl.user_info_by_username(userName).media_count
-        media_count = 100 if media_count>100 else media_count
+        media_count = self.cl.user_info_by_username(usern_name).media_count
+        media_count = 30 if media_count > 30 else media_count
 
-        # username으로 userid(int) 가져오기
-        user_id = int(self.cl.user_id_from_username(userName))
+        # user_name으로 userid(int) 가져오기
+        user_id = int(self.cl.user_id_from_username(usern_name))
 
         # 게시물 추출
         all_text = ""
@@ -40,13 +30,9 @@ class InstagramUtils:
         for post in posts:
             all_text += post.caption_text
 
-        print("===========================")
-        print(all_text)
-
         return [all_text]
 
-
-    def post_by_postUrl(self, url):
+    def post_by_post_url(self, url):
         # 게시물 텍스트 추출
         code = self.cl.media_pk_from_url(url)
         caption_text = self.cl.media_info(code).caption_text
