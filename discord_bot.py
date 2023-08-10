f127ebed0853150da15d8f3cf0cb6a59d8f8a223
juns1s <juns1s@naver.com>
2023-08-10 20:39:34 +0900

    chore: 파이썬 컨벤션에 맞게 수정

--- discord_bot.py
+++ discord_bot.py
@@ -3,6 +3,7 @@ import os
 import discord
 from dotenv import load_dotenv
 
+
 # 디스코드로 에러 전송 (봇)
 def send_error_to_discord(error_message):
     # 환경변수 가져오기
@@ -16,7 +17,7 @@ def send_error_to_discord(error_message):
     async def on_ready():
         channel = client.get_channel(int(channel_id))
         if channel:
-            await channel.send(str(datetime.datetime.now())+"\n"+str(error_message))
+            await channel.send(str(datetime.datetime.now()) + "\n" + str(error_message))
             await client.close()
         else:
             print("디스코드 채널을 찾을 수 없습니다")
